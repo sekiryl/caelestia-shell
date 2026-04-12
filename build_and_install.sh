9712415b5bd282e4afcfaaec44d39407600ef776
Sekiryl <114310783+sekiryl@users.noreply.github.com>
2026-04-12 22:35:34 +0530

    fix: version in build_and_install script

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,3 +1,3 @@
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/ -DVERSION=1.0.0
 cmake --build build
 sudo cmake --install build
